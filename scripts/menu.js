ce6f1aada1134ac709e4685f4841116779cabf45
Pedro Henrique Paulino Pires <121137670+pedropaulinopires@users.noreply.github.com>
2024-09-09 23:58:38 -0300

    Ajustando menu

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,18 +1,26 @@
 let btnMenu = document.getElementById("btn-menu");
+let btnMenuClose = document.getElementById("btn-close-menu");
 let meunContent = document.getElementById("menu-mobile-content");
 let divsMenuMobile = [...btnMenu.children];
-let itensMenu = [...document.querySelectorAll("#menu-mobile-content a")];
+let itensMenu = [...document.querySelectorAll(".item-menu")];
 
 let ativarDesativarMenu = () => {
   btnMenu.classList.toggle("active");
   meunContent.classList.toggle("active");
   document.body.classList.toggle("remove-scrolling");
-  itensMenu.forEach((ele) =>{
-    ele.classList.toggle("active")
+  itensMenu.forEach((ele, index) => {
+    if (ele.classList.contains('active')) {
+      ele.classList.remove('active')
+    } else {
+      setTimeout(() => {
+        ele.classList.add('active');
+      }, index * 200);
+    }
   })
 }
 
 btnMenu.addEventListener("click", () => ativarDesativarMenu());
+btnMenuClose.addEventListener("click", () => ativarDesativarMenu());
 
 itensMenu.forEach((ele) => {
   ele.addEventListener("click", () => {
@@ -20,11 +28,11 @@ itensMenu.forEach((ele) => {
   });
 });
 
-window.addEventListener('resize', () =>{
+window.addEventListener('resize', () => {
   btnMenu.classList.remove("active");
   meunContent.classList.remove("active");
   document.body.classList.remove("remove-scrolling");
-  itensMenu.forEach((ele) =>{
+  itensMenu.forEach((ele) => {
     ele.classList.remove("active")
   })
 });
